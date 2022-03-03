dabf81d462840deff7161a465ee37eb657673d54
Spandan Das Barman <spandandb94@gmail.com>
2022-03-03 21:41:05 +0530

    Admin Entity Completed

--- src/main/java/com/thinkxfactor/springdemo/entity/Admin.java
+++ src/main/java/com/thinkxfactor/springdemo/entity/Admin.java
@@ -1,8 +1,35 @@
 package com.thinkxfactor.springdemo.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tbl_admin")
 public class Admin {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "uid")
+    private Long uid;
+
+    public Long getUid() {
+        return uid;
+    }
+
+    public void setUid(Long uid) {
+        this.uid = uid;
+    }
+
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "username")
     private String username;
+
+    @Column(name = "password")
     private String password;
 
     public String getName() {
