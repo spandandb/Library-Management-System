1a838c91c4d3f1403a51d0095ddb7eba242a3db6
Spandan Das Barman <spandandb94@gmail.com>
2022-03-04 20:31:45 +0530

    User Rest API Completed

--- src/main/java/com/thinkxfactor/springdemo/controllers/UserController.java
+++ src/main/java/com/thinkxfactor/springdemo/controllers/UserController.java
@@ -1,9 +1,17 @@
 package com.thinkxfactor.springdemo.controllers;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 
+import com.thinkxfactor.springdemo.entity.User;
+import com.thinkxfactor.springdemo.repository.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,33 +19,61 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 public class UserController {
 
-    int count = 0;
+    // int count = 0;
 
-    @GetMapping("/")
-    public String error() {
-        return "ERROR from User Controller";
+    // @GetMapping("/")
+    // public String error() {
+    // return "ERROR from User Controller";
+    // }
+
+    // @GetMapping("/counter")
+    // public String count() {
+    // count = count + 1;
+    // return "The counter value is : " + count;
+    // }
+
+    // @GetMapping("/hello")
+    // public String sayHello() {
+    // return "This is a new server running!!";
+
+    // }
+
+    // @GetMapping("/hello2")
+    // public Map<String, String> sayHello2() {
+    // Map<String, String> respMap = new HashMap<>();
+    // respMap.put("name", "raj");
+    // respMap.put("age", "25");
+    // respMap.put("dept", "CSE");
+    // return respMap;
+
+    // }
+
+    @Autowired
+    UserRepository userRepository;
+
+    @PostMapping("/addUser")
+    public User addUser(@RequestBody User user) {
+        return userRepository.save(user);
     }
 
-    @GetMapping("/counter")
-    public String count() {
-        count = count + 1;
-        return "The counter value is : " + count;
+    @DeleteMapping("/deleteUser/{uid}")
+    public void deleteUser(@PathVariable Long uid) {
+        userRepository.deleteById(uid);
     }
 
-    @GetMapping("/hello")
-    public String sayHello() {
-        return "This is a new server running!!";
-
-    }
-
-    @GetMapping("/hello2")
-    public Map<String, String> sayHello2() {
-        Map<String, String> respMap = new HashMap<>();
-        respMap.put("name", "raj");
-        respMap.put("age", "25");
-        respMap.put("dept", "CSE");
-        return respMap;
+    @PutMapping("/updateUser")
+    public User updateUser(@RequestBody User User) {
+        return userRepository.save(User);
 
     }
 
+    @GetMapping("/getAllUser")
+    public List<User> getAllUser() {
+        return userRepository.findAll();
+    }
+
+    @GetMapping("getUser/{uid}")
+    public User getUser(@PathVariable Long uid) {
+        return userRepository.findById(uid).get();
+    }
 }
